7a10aec646fef424d0d76ef2ff38a33d4e7ff38e
atancredi <alessandro.tancredi@aitechnologies.it>
2023-05-15 13:06:20 +0200

    logic fixes on comparison script

--- comparison.py
+++ comparison.py
@@ -2,13 +2,6 @@ from os import listdir
 import json
 from datetime import datetime
 
-# load last 2 results
-last2results = []
-for file in listdir("results")[-2:]:
-    if file.endswith(".json"):
-        with open(f"results/{file}", "r") as f:
-            last2results.append(json.loads(f.read()))
-
 # get least and most recent results
 def get_date_from_iso(iso):
     if type(iso).__name__ == "datetime" or "T" not in iso:
@@ -24,31 +17,20 @@ def get_date_from_iso(iso):
     minute = __[1]
     return datetime(int(year), int(month), int(day), int(hour), int(minute))
 
-leastRecent = None
-mostRecent = None
-for i in last2results:
 
-    date = get_date_from_iso(i["date"])
-    
-    if leastRecent == None:
-        leastRecent = i["date"]
-    else:
-        if date < get_date_from_iso(leastRecent):
-            leastRecent = i["date"]
-        else:
-            if mostRecent == None:
-                mostRecent = i["date"]
-            elif date > mostRecent:
-                mostRecent = i["date"]
+# Load all results
+resultsData = []
+for file in listdir("results"):
+    if file.endswith(".json") and not file.startswith("_"):
+        with open(f"results/{file}", "r") as f:
+            resultsData.append(json.loads(f.read()))
 
-# get actual data
-leastRecentData = None
-mostRecentData = None
-for i in last2results:
-    if i["date"] == leastRecent:
-        leastRecentData = i
-    if i["date"] == mostRecent:
-        mostRecentData = i
+# Sort by date
+resultsData.sort(key=lambda x: get_date_from_iso(x["date"]), reverse=False)
+
+# get actual data from most recent and 2nd most recent scan
+leastRecentData = resultsData[-2]
+mostRecentData = resultsData[-1]
 
 # get gained followers
 gainedFollowers = []
@@ -58,16 +40,19 @@ for fl in mostRecentData["followers"]:
 
 # get lost followers
 # check if lost followers are really lost follower or deactivated/removed accounts
-from web import get_mac_browser,is_user_active
-browser = get_mac_browser()
+
 lostFollowers = []
 for fl in leastRecentData["followers"]:
     if fl["username"] not in [i["username"] for i in mostRecentData["followers"]]:
+        lostFollowers.append(fl)
+
+if len(lostFollowers) > 0:
+    from web import get_mac_browser,is_user_active
+    browser = get_mac_browser()
+    for fl in lostFollowers:
         if not is_user_active(browser,fl["username"]):
             fl["status"] = "deactivated"
         else: fl["status"] = "active"
-
-        lostFollowers.append(fl)
 
 # TODO check if a lost follower has the same bio of a gained follower - it can only have changed account (especially if says deactivated)
 
