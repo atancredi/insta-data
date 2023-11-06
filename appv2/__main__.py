7198fd1804a4749ab8a9902c53c3b8c25c9c1f78
atancredi <alessandro.tancredi@aitechnologies.it>
2023-11-06 01:51:55 +0100

    new version - functioning

--- appv2/__main__.py
+++ appv2/__main__.py
@@ -1,106 +1,22 @@
-from json import dumps
-from os.path import exists
+from my_stacklogging import new_logger
+from scan_engine import scan
+from scan_comparison import ScanComparison
 
-from cookies_config import load_cookies
-
-from seleniumrequests import Chrome
-from selenium.webdriver import ChromeOptions
-
-from data_models import ScanData
-
-def get_data():
-    USERNAME = "asciughino_"
-
-    options = ChromeOptions()
-
-    options.set_capability('goog:loggingPrefs', { 'browser':'ALL' })
-
-    options.add_argument("--headless")
-
-    webdriver = Chrome(options=options)
-    webdriver.get("https://www.instagram.com/")
-    if exists("cookies/cookies.pkl"):
-        for c in load_cookies():
-            webdriver.add_cookie(c)
-        webdriver.get("https://www.instagram.com/")
-    
-    _userdata = webdriver.request("GET",f"https://www.instagram.com/web/search/topsearch/?query={USERNAME}")
-    userdata = _userdata.json()
-    if _userdata.status_code != 200:
-        raise ConnectionError(f"({_userdata.status_code}) Could not fetch user data: "+dumps(userdata))
-
-    # the first result should be the matching one
-    user_id = userdata["users"][0]["user"]["pk"]
-
-    # optionally dump the results of the search
-    # dump(userdata, open("asciughino.json", "w"), indent=4)
-
-    print(f"Fetching data for user {USERNAME} with id {user_id}")
-    
-    followers = []
-    after = None
-    has_next = True
-    while has_next:
-        url = "https://www.instagram.com/graphql/query/"
-        params = {
-            "query_hash": "c76146de99bb02f6415203be841dd25a",
-            "variables": dumps({
-                "id": user_id,
-                "include_reel": True,
-                "fetch_mutual": True,
-                "first": 50,
-                "after": after
-            })
-        }
-        response = webdriver.request("GET", url, params=params)
-        data = response.json()
-        has_next = data["data"]["user"]["edge_followed_by"]["page_info"]["has_next_page"]
-        after = data["data"]["user"]["edge_followed_by"]["page_info"]["end_cursor"]
-        followers += [
-            {
-                "username": node["node"]["username"],
-                "full_name": node["node"]["full_name"]
-            }
-            for node in data["data"]["user"]["edge_followed_by"]["edges"]
-        ]
-    
-    print(len(followers))
-
-    after = None
-    has_next = True
-    followings = []
-
-    while has_next:
-        url = "https://www.instagram.com/graphql/query/"
-        params = {
-            "query_hash": "d04b0a864b4b54837c0d870b0e77e076",
-            "variables": dumps({
-                "id": user_id,
-                "include_reel": True,
-                "fetch_mutual": True,
-                "first": 50,
-                "after": after
-            })
-        }
-        response = webdriver.request("GET", url, params=params)
-        data = response.json()
-        has_next = data["data"]["user"]["edge_follow"]["page_info"]["has_next_page"]
-        after = data["data"]["user"]["edge_follow"]["page_info"]["end_cursor"]
-        followings += [
-            {
-                "username": node["node"]["username"],
-                "full_name": node["node"]["full_name"]
-            }
-            for node in data["data"]["user"]["edge_follow"]["edges"]
-        ]
-
-    print(len(followings))
-
-    dont_follow_me_back = [following for following in followings if not any(follower["username"] == following["username"] for follower in followers)]
-    i_dont_follow_back = [follower for follower in followers if not any(following["username"] == follower["username"] for following in followings)]
-
-    return ScanData(followers, followings, dont_follow_me_back, i_dont_follow_back)
+from traceback import format_exc
 
 if __name__ == "__main__":
-    scan_data = get_data()
-    scan_data.save_to_file()
+    log = new_logger()
+    try:
+        scan_data = scan("asciughino_", log)
+        scan_data.save_to_file()
+        log.info("Finished Scan")
+
+        engine = ScanComparison()
+        engine.load_from_files()
+        scan_comparison_data = engine.compare_scans()
+        scan_comparison_data.save_to_file()
+        log.info("Finished Comparison")
+
+    except Exception as ex:
+        log.error(f"{ex.__class__.__name__}: {ex}")
+        print(format_exc())
--- appv2/data_models.py
+++ appv2/data_models.py
@@ -1,31 +1,113 @@
 from datetime import datetime
+from typing import List, Optional
+from datetime import datetime
 from json import dump
 
-class ScanData:
-    followers: list
-    followings: list
-    dont_follow_me_back: list
-    i_dont_follow_back: list
-    date: str
+def get_date_from_iso(iso: str | datetime):
+    if type(iso).__name__ == "datetime" or "T" not in iso:
+        return iso
 
-    def __init__(self, followers: list, followings: list, dont_follow_me_back: list, i_dont_follow_back: list) -> None:
-        self.followers = followers
-        self.followings = followings
-        self.dont_follow_me_back = dont_follow_me_back
-        self.i_dont_follow_back = i_dont_follow_back
-       
-        self.date = datetime.now().isoformat()
+    _ = iso.split("T")
+    __ = _[0].split("-")
+    year = __[0]
+    month = __[1]
+    day = __[2]
+    __ = _[1].split(":")
+    hour = __[0]
+    minute = __[1]
+    return datetime(int(year), int(month), int(day), int(hour), int(minute))
+
+class Follower:
+    username: str
+    full_name: str
     
     @property
     def __dict__(self):
         return {
-            "date": self.date,
-            "followers": self.followers,
-            "followings": self.followings,
-            "dontFollowMeBack": self.dont_follow_me_back,
-            "iDontFollowBack": self.i_dont_follow_back,
+            "username": self.username,
+            "full_name": self.full_name
+        }
+    
+    def __init__(self,object: dict):
+        self.username=object["username"]
+        self.full_name=object["full_name"]
+
+class ScanData:
+    followers: List[Follower]
+    followings: List[Follower]
+    dont_follow_me_back: List[Follower]
+    i_dont_follow_back: List[Follower]
+    date: datetime
+
+    def set_date(self,date):
+        if not date:
+            self.date = datetime.now()
+        elif isinstance(date, str):
+            self.date = get_date_from_iso(date)
+        elif isinstance(date, datetime):
+            self.date = date
+
+    def load_from_raw(self, followers: List[Follower], followings: List[Follower], dont_follow_me_back: List[Follower], i_dont_follow_back: List[Follower], date: Optional[datetime] = None) -> None:
+        self.set_date(date)
+        self.followers = followers
+        self.followings = followings
+        self.dont_follow_me_back = dont_follow_me_back
+        self.i_dont_follow_back = i_dont_follow_back
+        return self
+        
+    def load_from_dict(self, object: dict):
+        self.set_date(object["date"])
+        self.followers = [Follower(x) for x in object["followers"]]
+        self.followings = [Follower(x) for x in object["followings"]]
+        self.dont_follow_me_back = [Follower(x) for x in object["dont_follow_me_back"]]
+        self.i_dont_follow_back = [Follower(x) for x in object["i_dont_follow_back"]]
+        return self
+
+    @property
+    def __dict__(self):
+        return {
+            "followers": [follower.__dict__ for follower in self.followers],
+            "followings": [follower.__dict__ for follower in self.followings],
+            "dont_follow_me_back": [follower.__dict__ for follower in self.dont_follow_me_back],
+            "i_dont_follow_back": [follower.__dict__ for follower in self.i_dont_follow_back],
+            "date": self.date.isoformat(),
         }
 
     def save_to_file(self):
-        now = datetime.now().strftime("%Y_%m_%d-%H_%M")
+        now = self.date.strftime("%Y_%m_%d-%H_%M")
         dump(self.__dict__, open("results/result_"+now+".json","w"), indent=4)
+
+class TimeObject:
+    reference_scan: datetime
+    current_scan: datetime
+
+    def __init__(self, reference_scan: datetime, current_scan: datetime) -> None:
+        self.reference_scan = reference_scan
+        self.current_scan = current_scan
+
+class ProfileData(Follower):
+    
+    def __init__(self,object: dict) -> None:
+        super().__init__(object)
+    
+class ScanComparisonData:
+
+    time_object: TimeObject
+    profile_data: ProfileData
+    gained_followers: List[Follower]
+    lost_followers: List[Follower]
+
+    @property
+    def __dict__(self):
+        return {
+            "time_object": {
+                "reference_scan": self.time_object.reference_scan.isoformat(),
+                "current_scan": self.time_object.current_scan.isoformat()
+            },
+            "gained_followers": [x.__dict__ for x in self.gained_followers],
+            "lost_followers": [x.__dict__ for x in self.lost_followers],
+        }
+
+    def save_to_file(self):
+        d = datetime.now().strftime("%Y_%m_%d-%H_%M")
+        dump(self.__dict__, open("results_comparisons/result_"+d+".json","w"), indent=4)
